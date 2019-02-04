0edc403c78b337534054c6bd5f71cb548be90ea8
TonyKat <anton.kalaturskii@yandex.ru>
2019-02-04 21:13:23 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+RESOURCE = 'https://graph.microsoft.com'
+TENANT = '<TENANT>'
+AUTHORITY_HOST_URL = 'https://login.microsoftonline.com'
+CLIENT_ID = '<CLIENT_ID>'
+CLIENT_SECRET = '<CLIENT_SECRET>'
+
+# These settings are for the Microsoft Graph API Call
+API_VERSION = 'v1.0'
